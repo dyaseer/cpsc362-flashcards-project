549642c2b0e61812e6bc0a9aa3a036766471e439
dyaseer <dyaseer06@gmail.com>
2026-09-22 14:46:30 +0000

    fixed test case to include hint

--- test.js
+++ test.js
@@ -58,7 +58,7 @@ class FlashDeck {
 function test() {
     console.log("Running test...");
     
-    newCard = new Flashcard("Destiny", "a predetermined course of events often held to be an irresistible power or agency");
+    newCard = new Flashcard("Destiny", "a predetermined course of events often held to be an irresistible power or agency", "Fate.");
 
     newDeck = new FlashDeck("deck1");
 
