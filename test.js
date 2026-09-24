3ceb0e064672c6f02a9f0b17df684e9f58a78638
BRENNANS-PC\Brennan <castillobrennan64@gmail.com>
2026-09-23 22:21:37 -0700

    Displays Created Flashcard

--- test.js
+++ test.js
@@ -55,25 +55,51 @@ class FlashDeck {
 
 }
 
-function test() {
-    console.log("Running test...");
+const testDeck = new FlashDeck("Test");
+const form = document.getElementById('flashcard-form');
+const container = document.getElementById('deck-container');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const termValue = document.getElementById('term-input').value;
+    const defValue = document.getElementById('def-input').value;
+
+    const newCard = new Flashcard(termValue, defValue, "");
+    testDeck.addcard(newCard);
+
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'flashcard-card';
+
+    cardDiv.innerHTML = `
+        <h2>${newCard.term}</h3>
+        <p>${newCard.def}</p>
+    `;
+
+    container.appendChild(cardDiv);
+
+    form.reset();
+});
+
+// function test() {
+//     console.log("Running test...");
     
-    newCard = new Flashcard("Destiny", "a predetermined course of events often held to be an irresistible power or agency", "Fate.");
+//     newCard = new Flashcard("Destiny", "a predetermined course of events often held to be an irresistible power or agency", "Fate.");
 
-    newDeck = new FlashDeck("deck1");
+//     newDeck = new FlashDeck("deck1");
 
-    console.log(newDeck.name);
+//     console.log(newDeck.name);
 
-    newDeck.addcard(newCard);
+//     newDeck.addcard(newCard);
 
-    console.log(newDeck.deck.get("Destiny"))
+//     console.log(newDeck.deck.get("Destiny"))
 
-    newDeck.removecard(newCard);
-
-
-    console.log("Test finished.");
+//     newDeck.removecard(newCard);
 
 
-}
+//     console.log("Test finished.");
 
-test();
+
+// }
+
+// test();
